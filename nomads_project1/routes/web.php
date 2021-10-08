f149c663f4d45f7d044dca1553b275c169805fbf
yusuf <yusuffront@gmail.com>
2021-10-08 17:18:02 +0800

    add file ui-public, create views folder and controller

--- nomads_project1/routes/web.php
+++ nomads_project1/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
