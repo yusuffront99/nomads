7e239188b079d478784be1fe944cea8363cd86db
yusuf <yusuffront@gmail.com>
2021-10-09 21:31:23 +0800

    create new templates success

---
+++ nomads_project1/resources/views/layouts/success.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+    @stack('prepend-style')
+    @include('includes.style')
+    @stack('addon-style')
+</head>
+<body>
+    
+    <!-- Navbar -->
+    @include('includes.navbar-alternative')
+    @yield('container')
+
+    <!-- Footer -->
+    @include('includes.footer')
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+</body>
+</html>
---
+++ nomads_project1/resources/views/pages/success.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.success')
+
+@section('container')
+    <!-- Main -->
+    <main>
+        <div class="section-success d-flex align-item-center">
+            <div class="col text-center">
+                <img src="frontend/images/ic_mail.png" alt="">
+                <h1>Yay! Success</h1>
+                <p>
+                    We’ve sent you email for trip instruction 
+                    <br> 
+                    please read it as well
+                </p>
+                <a href="index.html" class="btn btn-home-page mt-3 px-5">Home Page</a>
+            </div>
+        </div>
+    </main>
+@endsection
+
+@push('prepend-style')
+    <link rel="stylesheet" href="{{url('frontend/libraries/gijgo/css/gijgo.min.css')}}">
+@endpush
+
+@push('addon-script')
+<script src="{{url('frontend/libraries/xzoom/dist/xzoom.min.js')}}"></script>
+<script>
+    $(document).ready(function(){
+            // date picker
+            $('.datepicker').datepicker({
+                uiLibrary: 'bootstrap4',
+                icons : {
+                    rightIcon : '<img src="{{url("frontend/images/ic_doe.png")}}">'
+                }
+            })
+        })
+</script>
+@endpush
+@show
