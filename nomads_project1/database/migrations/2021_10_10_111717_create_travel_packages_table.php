e59cf108c44d0444b32cf4037cc4ec3713ef24e0
yusuf <yusuffront@gmail.com>
2021-10-10 19:34:23 +0800

    create new table travel packages

---
+++ nomads_project1/database/migrations/2021_10_10_111717_create_travel_packages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('loccation');
+            $table->longText('about');
+            $table->string('featured_event');
+            $table->string('language');
+            $table->string('foods');
+            $table->date('dapartured_date');
+            $table->string('duration');
+            $table->string('type');
+            $table->integer('price');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_packages');
+    }
+}
