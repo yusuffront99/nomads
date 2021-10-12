c82f3b9cc0a05a697875948f792a0dd48cae6851
yusuf <yusuffront@gmail.com>
2021-10-12 16:13:52 +0800

    create new model TravelPackage

---
+++ nomads_project1/app/Models/TravelPackage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TravelPackage extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $hidden = [
+
+    ];
+}
