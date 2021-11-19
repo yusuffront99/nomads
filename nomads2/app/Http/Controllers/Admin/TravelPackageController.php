816bb38b6918cb2cd94b02b98fce17dd6fcfecb8
yusuf <yusuffront@gmail.com>
2021-11-19 14:44:18 +0800

    create new form update travel package

--- nomads2/app/Http/Controllers/Admin/TravelPackageController.php
+++ nomads2/app/Http/Controllers/Admin/TravelPackageController.php
@@ -70,7 +70,11 @@ class TravelPackageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+
+        return view('pages.admin.travel-package.edit',[
+            'item' => $item
+        ]);
     }
 
     /**
@@ -80,9 +84,15 @@ class TravelPackageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TravelPackageRequest $request, $id)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($request->title);
+
+        $item = TravelPackage::findOrFail($id);
+        $item->update($data);
+
+        return redirect()->route('travel-package.index');
     }
 
     /**
