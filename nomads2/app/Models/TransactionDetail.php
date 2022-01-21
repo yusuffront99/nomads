6b6576f356641a4de29e54979c2d206c07a8bc64
yusuf <yusuffront@gmail.com>
2022-01-21 18:07:06 +0800

    create new model transaction_details and new request

---
+++ nomads2/app/Models/TransactionDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id', 'username', 'nationality','is_visa', 'doe_passport'
+    ];
+
+
+    //Relationship
+
+    public function transaction(){
+        return $this->belongsTo(Transaction::class, 'transactions_id','id');
+    }
+
+}
